422d0b595f28f3bf2983f37007a78ed8b38f9c89
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2015-01-08 14:34:04 +0200

    testing otr stream

--- examples/t-client.js
+++ examples/t-client.js
@@ -1,20 +1,20 @@
 var transport = require("../lib/transport");
 
 transport.account({
-    name: 'client'
+	name: 'client'
 }, function (account) {
-    if (!account) {
-        console.log("error-no account");
-        return;
-    }
-    client = transport.createClient({
-        name: "client",
-        account: account,
-        connect: function (streams, ipaddress, port, fingerprint) {
-            //process.stdin.pipe(streams.otr_out);
-            //streams.otr_in.pipe(process.stdout);
-            process.stdin.pipe(streams.aes_out);
-            streams.aes_in.pipe(process.stdout);
-        }
-    }).connect("127.0.0.1", 6666);
+	if (!account) {
+		console.log("error-no account");
+		return;
+	}
+	client = transport.createClient({
+		name: "client",
+		account: account,
+		connect: function (streams, ipaddress, port, fingerprint) {
+			process.stdin.pipe(streams.otr_out);
+			streams.otr_in.pipe(process.stdout);
+			//process.stdin.pipe(streams.aes_out);
+			//streams.aes_in.pipe(process.stdout);
+		}
+	}).connect("127.0.0.1", 6666);
 });
--- examples/t-server.js
+++ examples/t-server.js
@@ -1,23 +1,23 @@
 var transport = require("../lib/transport");
 
 transport.account({
-    name: "server"
+	name: "server"
 }, function (account) {
-    if (!account) {
-        console.log("error-no account");
-        return;
-    }
-    transport.createServer({
-        name: "server",
-        account: account,
-        connect: function (streams, ipaddress, port, fingerprint) {
-            console.log("client connected:", fingerprint, "from:", ipaddress, port);
+	if (!account) {
+		console.log("error-no account");
+		return;
+	}
+	transport.createServer({
+		name: "server",
+		account: account,
+		connect: function (streams, ipaddress, port, fingerprint) {
+			console.log("client connected:", fingerprint, "from:", ipaddress, port);
 
-            //streams.otr_in.pipe(process.stdout);
-            //process.stdout.pipe(streams.otr_out);
-            streams.aes_in.pipe(process.stdout);
-            process.stdin.pipe(streams.aes_out);
-        },
-        acl_fingerprints: ['123abc', '456def'] //not functional yet
-    }).listen(6666);
+			streams.otr_in.pipe(process.stdout);
+			process.stdout.pipe(streams.otr_out);
+			//streams.aes_in.pipe(process.stdout);
+			//process.stdin.pipe(streams.aes_out);
+		},
+		acl_fingerprints: ['123abc', '456def'] //not functional yet
+	}).listen(6666);
 });
