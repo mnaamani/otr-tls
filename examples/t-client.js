4d55b8dc26521009feb6e7641abdf4aab06e9403
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2015-01-31 22:39:45 +0200

    better streams, clearer code, updated to use latest enet 0.2.1

--- examples/t-client.js
+++ examples/t-client.js
@@ -4,17 +4,22 @@ transport.account({
 	name: 'client'
 }, function (account) {
 	if (!account) {
-		console.log("error-no account");
+		console.log("error-no-account");
 		return;
 	}
-	client = transport.createClient({
-		name: "client",
+	console.log("our fingerprint:", account.fingerprint());
+
+	var client = transport.createClient({
 		account: account,
+		//todo pass this function as a callback paramter in client.connect() method
+		//so we can handle timeouts...
+		//and return one object with streams,address and fingerprint
 		connect: function (streams, ipaddress, port, fingerprint) {
-			process.stdin.pipe(streams.otr_out);
-			streams.otr_in.pipe(process.stdout);
-			//process.stdin.pipe(streams.aes_out);
-			//streams.aes_in.pipe(process.stdout);
+			console.log("connection established: %s:%s (%s)", ipaddress, port, fingerprint);
+			process.stdin.pipe(streams.otr).pipe(process.stdout);
+			//process.stdin.pipe(streams.aes).pipe(process.stdout);
 		}
-	}).connect("127.0.0.1", 6666);
+	});
+	console.log("connecting...");
+	client.connect("127.0.0.1", 6666);
 });
--- examples/t-server.js
+++ examples/t-server.js
@@ -7,17 +7,32 @@ transport.account({
 		console.log("error-no account");
 		return;
 	}
-	transport.createServer({
-		name: "server",
+	console.log("our fingerprint:", account.fingerprint());
+	var server = transport.createServer({
 		account: account,
+		port: 6666,
+		maxpeers: 32,
+		//todo need to return a peer object - to manullay disconnect when dont processing streams
+		//or should be disconnect when all streams end?
 		connect: function (streams, ipaddress, port, fingerprint) {
-			console.log("client connected:", fingerprint, "from:", ipaddress, port);
+			console.log("accepted connection: %s:%s (%s)", ipaddress, port, fingerprint);
+			//process.stdin.pipe(streams.otr).pipe(process.stdout);
+			//process.stdin.pipe(streams.aes).pipe(process.stdout);
+			streams.otr.pipe(process.stdout);
+			streams.aes.pipe(process.stdout);
 
-			streams.otr_in.pipe(process.stdout);
-			process.stdout.pipe(streams.otr_out);
-			//streams.aes_in.pipe(process.stdout);
-			//process.stdin.pipe(streams.aes_out);
+			streams.otr.on("end", function () {
+				console.log("OTR stream ended with: %s:%s", ipaddress, port);
+			});
+
+			streams.aes.on("end", function () {
+				console.log("AES stream ended with: %s:%s", ipaddress, port);
+			});
 		},
 		acl_fingerprints: ['123abc', '456def'] //not functional yet
-	}).listen(6666);
+	});
+
+	server.listen(function (err) {
+		console.log(err || "server ready.");
+	});
 });
